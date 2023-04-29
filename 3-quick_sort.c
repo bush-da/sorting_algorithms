66b260e52dafbf19435da3fc4bbc2ed10dfdaefa
bush-da <danielsevani12@gmail.com>
2023-04-29 11:32:49 -0700

    implement quick sort algorithm using lomuto partition schema

---
+++ 3-quick_sort.c
@@ -0,0 +1,93 @@
+#include "sort.h"
+
+void swap(int *a, int *b);
+int partition(int *array, size_t size, int left, int right);
+void sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
+
+/**
+ * swap - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * partition - the lomuto partition scheme (last element as pivot).
+ * @array: The array of integers.
+ * @size: The size of the array.
+ * @left: The starting index of the subset to order.
+ * @right: The ending index of the subset to order.
+ * Return: The final partition index.
+ */
+int partition(int *array, size_t size, int left, int right)
+{
+	int *pivot, above, below;
+
+	pivot = array + right;
+	for (above = below = left; below < right; below++)
+	{
+		if (array[below] < *pivot)
+		{
+			if (above < below)
+			{
+				swap(array + below, array + above);
+				print_array(array, size);
+			}
+			above++;
+		}
+	}
+
+	if (array[above] > *pivot)
+	{
+		swap(array + above, pivot);
+		print_array(array, size);
+	}
+
+	return (above);
+}
+
+/**
+ * sort - Implement the quicksort algorithm through recursion.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ * @left: The starting index of the array partition to order.
+ * @right: The ending index of the array partition to order.
+ * Return: void
+ */
+
+void sort(int *array, size_t size, int left, int right)
+{
+	int part;
+
+	if (right > left)
+	{
+		part = partition(array, size, left, right);
+		sort(array, size, left, part - 1);
+		sort(array, size, part + 1, right);
+	}
+}
+
+/**
+ * quick_sort - Sort an array using the quicksort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ * Return: void
+ * Description: Uses the Lomuto partition scheme. Prints
+ *              the array after each swap of two elements.
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	sort(array, size, 0, size - 1);
+}
